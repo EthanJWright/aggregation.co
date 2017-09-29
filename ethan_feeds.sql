9d6d18c5e0f92c1a8201ef34c2dcfec8ab7eb41b
Ethan Wright <ewright362@gmail.com>
2017-09-28 21:14:50 -0600

    current changes

--- ethan_feeds.sql
+++ ethan_feeds.sql
@@ -4,4 +4,5 @@ INSERT INTO feeds VALUES(3, 3, "https://www.cnet.com/rss/news/", "regular");
 INSERT INTO feeds VALUES(4, 3, "https://hnrss.org/frontpage", "forum");
 INSERT INTO feeds VALUES(5, 2, "http://www.reddit.com/.rss", "forum");
 INSERT INTO feeds VALUES(6, 3, "https://news.google.com/news/rss/headlines/section/topic/TECHNOLOGY?ned=us&hl=en", "regular");
+INSERT INTO feeds VALUES(7, 1, "https://www.reddit.com/r/mechanicalkeyboards/.rss", "forum");
 
